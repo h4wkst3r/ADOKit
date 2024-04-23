36f8df5756d57ac1f2f8c829d93e6a3a25ea5058
chryzsh <chryzsh@users.noreply.github.com>
2024-04-23 17:12:09 +0200

    oops

--- ADOKit/Utilities/UserUtils.cs
+++ ADOKit/Utilities/UserUtils.cs
@@ -91,12 +91,13 @@ namespace ADOKit.Utilities
 
                                         userList.Add(new User(directoryAlias, displayName, principalName, descriptor));
                                     }
-                                    
+                                    descriptor = "";
+                                    directoryAlias = "";
+                                    displayName = "";
+                                    principalName = "";
+
                                 }
-                                descriptor = "";
-                                directoryAlias = "";
-                                displayName = "";
-                                principalName = "";
+                                
 
 
                                 break;
